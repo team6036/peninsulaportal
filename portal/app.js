8595c5699af78207744f6883b1eeaf8d23fd6abc
jeffrey <jeffrey.fanjf@gmail.com>
2023-08-21 13:09:34 -0700

    App Menu Bar 2

--- portal/app.js
+++ portal/app.js
@@ -63,11 +63,8 @@ export default class App extends core.App {
 
         document.body.addEventListener("mousemove", e => {
             if (!this.hasEBackground()) return;
-            let x = e.pageX/window.innerWidth;
-            let y = e.pageY/window.innerHeight;
-            let scale = 0.1;
-            this.eBackground.style.setProperty("--x", (100*(scale*(x-0.5)+0.5))+"%");
-            this.eBackground.style.setProperty("--y", (100*(scale*(y-0.5)+0.5))+"%");
+            this.eBackground.style.setProperty("--x", e.pageX+"px");
+            this.eBackground.style.setProperty("--y", e.pageY+"px");
         });
 
         this.addHandler("start-begin", data => {
@@ -76,26 +73,53 @@ export default class App extends core.App {
         let ctx = null;
         this.addHandler("start-complete", data => {
             this.#eBackground = document.querySelector("#TITLEPAGE > .background");
-            this.#eCanvas = document.querySelector("#TITLEPAGE > #canvas");
+            if (this.hasEBackground()) {
+                let size = 50;
+                this.eBackground.style.setProperty("--size", size+"px");
+                let w = Math.ceil(window.screen.width / (Math.sqrt(3) * size))-5;
+                let h = Math.ceil(window.screen.height / (2 * size))-5;
+                w = h = Math.max(w, h);
+                let totalW = w * (Math.sqrt(3) * size);
+                let totalH = h * ((3/2) * size);
+                for (let x = 0; x <= w; x++) {
+                    for (let y = 0; y <= h; y++) {
+                        let icon = document.createElement("ion-icon");
+                        this.eBackground.appendChild(icon);
+                        let type = "solid";
+                        if (y == Math.floor(h/2)+3) {
+                            if (x == Math.floor(w/2)) type = 1;
+                            else if (x == Math.floor(w/2)+1) type = 2;
+                        }
+                        if (y == Math.floor(h/2)+2) {
+                            if (x == Math.floor(w/2)) type = 3;
+                            else if (x == Math.floor(w/2)+1) type = 4;
+                        }
+                        icon.setAttribute("src", `../assets/logo-part-${type}.svg`);
+                        icon.style.setProperty("--x", (((x+0.25) * (Math.sqrt(3) * size)) - ((y%2==0)?(Math.sqrt(3)/2):0)*size - totalW/2)+"px");
+                        icon.style.setProperty("--y", ((y * ((3/2) * size)) - totalH/2 + 20)+"px");
+                    }
+                }
+            }
+            this.#eCanvas = document.querySelector("#TITLEPAGE > .background > #canvas");
             if (this.hasECanvas())
                 ctx = this.eCanvas.getContext("2d");
             this.#eNav = document.querySelector("#TITLEPAGE > .inner > .nav");
             this.#eInfo = document.querySelector("#TITLEPAGE > .info");
             if (this.hasEInfo()) {
-                this.eInfo.innerHTML = "<div></div><div></div><div></div><div></div>";
-                this.eInfo.children[0].textContent = "NodeJS: "+window.version.node();
-                this.eInfo.children[1].textContent = "Chrome: "+window.version.chrome();
-                this.eInfo.children[2].textContent = "Electron: "+window.version.electron();
-                this.eInfo.children[3].innerHTML = "<div class='loading' style='--size:5px;--color:var(--v2);padding:5px;'></div>";
+                this.eInfo.innerHTML = "<div class='loading' style='--size:5px;--color:var(--v2);padding:5px;'></div>";
                 (async () => {
-                    let os = await window.version.os();
-                    let data = os.platform+" "+os.arch;
-                    if (os.cpus.length > 0) {
-                        let models = new Set(os.cpus.map(obj => obj.model));
-                        if (models.size > 1) data += " / CPUS: "+[...models].join(", ");
-                        else data += " / "+[...models][0];
+                    let about = await this.getAbout();
+                    this.eInfo.innerHTML = new Array(4).fill("<div></div>").join("");
+                    this.eInfo.children[0].textContent = "NodeJS: "+about.node;
+                    this.eInfo.children[1].textContent = "Chrome: "+about.chrome;
+                    this.eInfo.children[2].textContent = "Electron: "+about.electron;
+                    this.eInfo.children[3].textContent = "OS: "+about.os.platform+" "+about.os.arch;
+                    if (about.os.cpus.length > 0) {
+                        let models = [...new Set(about.os.cpus.map(obj => obj.model))];
+                        this.eInfo.children[3].textContent += " / ";
+                        if (models.length > 1) this.eInfo.children[4].textContent += "CPUS: "+models.join(", ");
+                        else this.eInfo.children[3].textContent += models[0];
                     }
-                    this.eInfo.children[3].textContent = "OS: "+data;
                 })();
             }
             
