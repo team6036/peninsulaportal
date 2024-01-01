5cebb161c6bca33ed5fec0c3e16001e3ca9a716b
jeffrey <jeffrey.fanjf@gmail.com>
2024-01-01 08:49:48 -0800

    Fix Portal/Presets UserAgent

--- src/portal/app.js
+++ src/portal/app.js
@@ -445,7 +445,7 @@ export default class App extends core.App {
                 });
             this.#eInfo = document.querySelector("#PAGE > .info");
             if (this.hasEInfo()) {
-                this.getAbout().forEach(line => {
+                this.getUserAgent().forEach(line => {
                     let elem = document.createElement("div");
                     this.eInfo.appendChild(elem);
                     elem.textContent = line;
